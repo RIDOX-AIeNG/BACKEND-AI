b9eeda16d269f92cb2275f0ea423c3864f0d4abb
Ridwan <ayomideridox@gmail.com>
2025-10-13 07:20:08 -0700

    classes in python

---
+++ main.py
@@ -0,0 +1,15 @@
+class Pet:
+    def __init__(self, name, age, species):
+        self.name = name
+        self.age = age
+        self.species = species
+
+    def display_info(self):
+        print(f"Name: {self.name} \nSpecies: {self.species} \nAge:{self.age}")
+
+    def celebrate_birthday(self):
+        print(f"Happy birthday: {self.name}")
+
+EkpoDavid = Pet('Ekpo',150,'Rat')
+EkpoDavid.display_info()
+EkpoDavid.celebrate_birthday()
