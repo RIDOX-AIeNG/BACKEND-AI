0deecd1cb8fa1ec81148cd58745c24b77a1d37d6
ridox ayomide <ayomideridox@gmail.com>
2025-10-16 12:07:03 +0100

    another commit

--- POSTMAN/delete.py
+++ POSTMAN/delete.py
@@ -1,30 +1,45 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
 import json
-
-data = {
-    
-}
+data = [{
+        "name":"Sam Larry",
+        "track":"AI Developer"
+    },
+    {
+        "name":"YONDA ASA",
+        "track":"AI Backend"
+    }]
 class BasicAPI(BaseHTTPRequestHandler):
-    def send_data(self, data, status = 200):
+    def send_data(self, payload, status = 200):
         self.send_response(status)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode())
-
-    def do_PUT(self):
-        contact_size = int(self.headers.get('COntent-Length', 0))
-        parsed_data = self.rfile.read(contact_size)
-
-        post_data = json.loads(parsed_data)
-        data.clear(post_data) #saving data to database
-        self.send_data({
-            "Message": "Data Recieved",
-            "data": post_data
-        })
-
+        self.wfile.write(json.dumps(payload).encode())
+    def do_DELETE(self):
+        if data:
+           data.clear()
+           self.send_data({
+            "Message":"Data Deleted",
+            "data": []
+            })
+        else:
+            self.send_data({
+                "Message": "There no data to delete"
+            })
 def run():
-    HTTPServer(('localhost', 5000), BasicAPI).serve_forever()
-
+    HTTPServer(("localhost", 5000), BasicAPI).serve_forever()
 print("Application is running")
 run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- POSTMAN/put.py
+++ POSTMAN/put.py
@@ -1,30 +1,43 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
 import json
-
-data = {
-    
-}
+data =[{
+        "name":"Sam Larry",
+        "track":"AI Developer"
+    },
+    {
+        "name":"YONDA ASA",
+        "track":"AI Backend"
+    }
+]
 class BasicAPI(BaseHTTPRequestHandler):
-    def send_data(self, data, status = 200):
+    def send_data(self, payload, status = 200):
         self.send_response(status)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode())
-
+        self.wfile.write(json.dumps(payload).encode())
     def do_PUT(self):
-        contact_size = int(self.headers.get('COntent-Length', 0))
-        parsed_data = self.rfile.read(contact_size)
-
-        post_data = json.loads(parsed_data)
-        data.update(post_data) #saving data to database
-        self.send_data({
-            "Message": "Data Recieved",
-            "data": post_data
+        cotent_size =int(self.headers.get("Content-Length",0))
+        parsed_data = self.rfile.read(cotent_size)
+        pUT_data = json.loads(parsed_data)
+        if data:
+            data[0] = pUT_data
+            self.send_data({
+            "Message":"Data Replaced",
+            "data":data[0]
         })
-
+        else:
+          data.append(pUT_data)
+          self.send_data({
+            "Message":"Data Addeded",
+            "data":pUT_data
+        })
 def run():
-    HTTPServer(('localhost', 5000), BasicAPI).serve_forever()
-
+    HTTPServer(("localhost", 5000), BasicAPI).serve_forever()
 print("Application is running")
 run()
+
+
+
+
+
+
