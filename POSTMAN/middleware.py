f88890634fa0b75b0a409d1ec6ba6764ae97cabb
ridox ayomide <ayomideridox@gmail.com>
2025-10-27 16:10:37 +0100

    commit

--- POSTMAN/middleware.py
+++ POSTMAN/middleware.py
@@ -1,1 +1,12 @@
 import jwt
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+load_dotenv()
+
+secret_key = os.getenv("secret_key")
+def create_token(details,expiry):
+    expire =datetime.now() + expiry
+
+    details.update
